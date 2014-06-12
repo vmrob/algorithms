9d9324b7b7015df3e12aeba0258fcce35c86b200
Victor Robertson <victor.robertson.iv@gmail.com>
2014-06-12 06:43:29 -0400

    add more checks

--- unittests/MaxFlow.cpp
+++ unittests/MaxFlow.cpp
@@ -34,6 +34,17 @@ TEST_SUITE("Max Flow") {
 
 			CHECK(path == Graph<int>::Path({0, 1, 2}));
 		}
+		{
+			Graph<int> g;
+
+			g.addBidirectionalEdge(0, 2, 1);
+			g.addBidirectionalEdge(2, 1, 1);
+			g.addBidirectionalEdge(1, 3, 1);
+
+			auto path = FindPath(g, 0, 3);
+
+			CHECK(path == Graph<int>::Path({0, 2, 1, 3}));
+		}
 	};
 
 	TEST("Basic Graphs") {
@@ -43,6 +54,39 @@ TEST_SUITE("Max Flow") {
 			g.addBidirectionalEdge(0, 1, 1);
 
 			CHECK(FordFulkersonMaxFlow(g, 0, 1) == 1);
+		}
+		{
+			Graph<int> g;
+
+			g.addBidirectionalEdge(0, 1, 1);
+			g.addBidirectionalEdge(1, 2, 2);
+
+			CHECK(FordFulkersonMaxFlow(g, 0, 2) == 1);
+		}
+		{
+			Graph<int> g;
+
+			g.addBidirectionalEdge(0, 1, 1);
+			g.addBidirectionalEdge(1, 2, 2);
+			g.addBidirectionalEdge(2, 3, 3);
+			g.addBidirectionalEdge(3, 4, 4);
+			g.addBidirectionalEdge(4, 0, 5);
+
+			CHECK(FordFulkersonMaxFlow(g, 0, 4) == 6);
+		}
+		{
+			// from http://en.wikipedia.org/wiki/Ford%E2%80%93Fulkerson_algorithm
+			// "integral example"
+			Graph<int> g;
+
+			g.addBidirectionalEdge(0, 1, 100);
+			g.addBidirectionalEdge(1, 2, 1);
+			g.addBidirectionalEdge(2, 3, 100);
+
+			g.addBidirectionalEdge(0, 2, 100);
+			g.addBidirectionalEdge(1, 3, 100);
+
+			CHECK(FordFulkersonMaxFlow(g, 0, 3) == 200);
 		}
 	};
 
